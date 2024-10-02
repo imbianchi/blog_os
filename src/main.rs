a8b2067a2a5d646fd9a9da03d446d89afcf50b2e
JP! <joaobianchi@live.com>
2024-10-02 20:38:33 +0100

    :construction: WIP: Add interrupt handling

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,35 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(blog_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
-mod vga_buffer;
+use blog_os::println;
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello World!";
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
 
+    blog_os::init();
+
+    x86_64::instructions::interrupts::int3();
+
+    #[cfg(test)]
+    test_main();
+
+    loop {}
+}
+
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     println!("{}", _info);
     loop {}
 }
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-    panic!("Some panic message");
-
-    loop {}
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    blog_os::test_panic_handler(info)
 }
